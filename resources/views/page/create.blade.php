1eba3ae79a8533a9143f5811f43e8692dd65df66
emyelabasy <egad86@yahoo.com>
2016-06-09 15:39:59 +0200

    update page

--- resources/views/page/create.blade.php
+++ resources/views/page/create.blade.php
@@ -12,7 +12,7 @@
         <br><br>
         {!!Form::open(['route'=>'page.store','method'=>'post']) !!}
 
-            <div class='form-group has-warning'>
+            <div class='form-group has-warning' id='ourpage'>
             <label class='col-md-2'>Select Your Page</label>
             <div class='col-md-10 input-group'>
                 <span class='input-group-addon'><i class='glyphicon glyphicon-flag'></i></span>
@@ -32,11 +32,11 @@
 
                 <!-- for display select between write address and use google map -->
         <div class='col-lg-offset-10 col-ms-2' id="sel_getpage"> 
-            <a id="getbage" class="btn btn-primary getbage">Enter Another Page  <span class="glyphicon glyphicon-modal-window"></span> </a>
+            <a id="id_getbage" class="btn btn-primary getbage">Enter Another Page  <span class="glyphicon glyphicon-modal-window"></span> </a>
             
         </div> 
         <br/>
-
+        <div id="enternewbage">
           <div class='form-group has-warning'>
             <label class='col-md-2'>Title Page</label>
             <div class='col-md-10 input-group'>
@@ -44,13 +44,33 @@
                 <input placeholder='PlZ,enter page title ...' class='form-control' name='title' type='text'/>
             </div>
         </div>     
-          <textarea name='content'></textarea>
+          <textarea  name='content'></textarea>
+       </div>   
           <br><br>
           <input type='submit' class='col-md-offset-1 col-md-10 btn btn-primary btn-lg' name='ok' value='ADD' />
       {!!Form::close() !!}
   </div><!--end leftsideof from-->
 </div>
+
+<meta name="_token" id='token' content="{!! csrf_token() !!}" />
+<script type="text/javascript" src="{{url('/assets/js/jquery-2.1.4.min.js')}}"></script>
+<script type="text/javascript" src="{{url('/assets/js/jquery-1.12.0.min.js')}}"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script>
+$(document).ready(function(){
+
+    console.log('hii');
+    document.getElementById('ourpage').style.display = "block";
+    document.getElementById('enternewbage').style.display = "none";
+    
+    $('.getbage').click(function() {
+      document.getElementById('ourpage').style.display = "none";
+      document.getElementById('enternewbage').style.display = "block";
+    });
+
+
+});
+
   var editor_config = {
     path_absolute : "/",
     selector: "textarea",
