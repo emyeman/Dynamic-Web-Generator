6a29cb3e71fa9c54d609e7c89c9c48024e20f585
emyelabasy <egad86@yahoo.com>
2016-06-17 03:44:00 +0200

    add modify on temp1 and add temp3

---
+++ resources/views/temp3/footer.blade.php
@@ -0,0 +1,30 @@
+
+		<!-- JavaScript files placed at the end of the document so the pages load faster
+		================================================== -->
+		<!-- Jquery and Bootstap core js files -->
+		<script type="text/javascript" src="{{ url('/assets/plugins/jquery.min.js')}}"></script>
+		<script type="text/javascript" src="{{ url('/assets/bootstrap/js/bootstrap.min.js')}}"></script>
+
+		<!-- Modernizr javascript -->
+		<script type="text/javascript" src="{{ url('/assets/plugins/modernizr.js')}}"></script>
+
+        <script type="text/javascript" src="{{ url('/assets/bootstrap-iconpicker/js/bootstrap-iconpicker.min.js') }}"></script>
+
+		<!-- Isotope javascript -->
+		<script type="text/javascript" src="{{ url('/assets/plugins/isotope/isotope.pkgd.min.js')}}"></script>
+		
+		<!-- Backstretch javascript -->
+		<script type="text/javascript" src="{{ url('/assets/plugins/jquery.backstretch.min.js')}}"></script>
+
+		<!-- Appear javascript -->
+		<script type="text/javascript" src="{{ url('/assets/plugins/jquery.appear.js')}}"></script>
+
+		<!-- Initialization of Plugins -->
+		<script type="text/javascript" src="{{ url('/assets/js/template.js')}}"></script>
+
+		<!-- Custom Scripts -->
+		<script type="text/javascript" src="{{ url('/assets/js/custom.js')}}"></script>
+
+
+	</body>
+</html>
