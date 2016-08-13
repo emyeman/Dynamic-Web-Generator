363c4729c80cfbf467c05d96098e825f81dc7480
emyelabasy <egad86@yahoo.com>
2016-08-13 14:46:45 +0200

    add profile image in register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -180,6 +180,25 @@
       </div>
 
 
+
+      <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+          <label for="image" class="col-md-5 control-label">Profile Image</label>
+
+          <div class="col-md-7">
+          <label class="btn btn-info form-control btn-file">
+              Browse <input id="image" type="file" name="image" style="display: none;">
+          </label>
+
+              @if ($errors->has('password_confirmation'))
+                  <span class="help-block">
+                      <strong>{{ $errors->first('password_confirmation') }}</strong>
+                  </span>
+              @endif
+          </div>
+      </div>
+
+<br/>
+
       <div class="row">
         <div class="col-xs-8">
           <div class="checkbox icheck">
